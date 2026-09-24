ad5f55e3be4f37a4e213f07186bb39428bec3543
radigy <31765939+radigy@users.noreply.github.com>
2026-09-24 15:24:28 +0200

    feat: create basic test case for jsonplaceholder

---
+++ tests/test_posts.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def test_get_post():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["id"] == 1
+    assert body["userId"] == 1
+    assert "title" in body
+    assert "body" in body
