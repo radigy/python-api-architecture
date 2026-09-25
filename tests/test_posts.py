4de9bd3ef7b623aecfa25b1d9b06d926a5bd88fd
radigy <31765939+radigy@users.noreply.github.com>
2026-09-25 14:42:44 +0200

    chore: extract configuration

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,9 +1,11 @@
 import requests
 
+from config import BASE_URL
+
 
 def test_get_post():
     response = requests.get(
-        "https://jsonplaceholder.typicode.com/posts/1"
+        f"{BASE_URL}/posts/1"
     )
 
     assert response.status_code == 200
