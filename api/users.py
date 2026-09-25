e6f493a871fbe1ffbeb8602e72d50a49cbdb4fd5
radigy <31765939+radigy@users.noreply.github.com>
2026-09-26 00:29:35 +0200

    feat: add UsersAPI class

---
+++ api/users.py
@@ -0,0 +1,11 @@
+class UsersAPI:
+
+    def __init__(self, client):
+        self.client = client
+
+    def get_user(self, user_id):
+        return self.client.get(f"/users/{user_id}")
+
+    def get_users(self):
+        return self.client.get("/users")
+    
